1d6b1ab096a35afdd1606d1ee53eb573c968ab65
Madeline Watts <madeline.chloe429@gmail.com>
2025-06-16 12:18:14 +0200

    about me ui

--- rootcause_app/lib/screens/profile.dart
+++ rootcause_app/lib/screens/profile.dart
@@ -17,11 +17,52 @@ class _ProfileScreenState extends State<ProfileScreen> {
           IconButton(
             onPressed: () => Navigator.pop(context),
             icon: Icon(Icons.close),
-            
           ),
         ],
       ),
-      body: SafeArea(child: Center(child: Text("Profile Screen"))),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "About Me",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  SizedBox(width: 6),
+                  GestureDetector(
+                    onTap: () {
+                      print('Info icon tapped, implement info modal');
+                    },
+                    child: Icon(Icons.info_outline, size: 18),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 12),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFF1F1F1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    hintText: 'Add conditions and known symptoms here...',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: InputBorder.none,
+                    isCollapsed: true,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
