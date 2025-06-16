518859f67ccb172af91778a4635ccb3567f148dc
Madeline Watts <madeline.chloe429@gmail.com>
2025-06-16 15:42:53 +0200

    account settings ui

--- rootcause_app/lib/screens/profile.dart
+++ rootcause_app/lib/screens/profile.dart
@@ -11,7 +11,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),
+        title: Text(
+          "Profile",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
         automaticallyImplyLeading: false,
         actions: [
           IconButton(
@@ -24,40 +27,108 @@ class _ProfileScreenState extends State<ProfileScreen> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
+              Column(
                 children: [
-                  Text(
-                    "About Me",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  SizedBox(width: 6),
-                  GestureDetector(
-                    onTap: () {
-                      print('Info icon tapped, implement info modal');
-                    },
-                    child: Icon(Icons.info_outline, size: 18),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "About Me",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          SizedBox(width: 6),
+                          GestureDetector(
+                            onTap: () {
+                              print('Info icon tapped, implement info modal');
+                            },
+                            child: Icon(Icons.info_outline, size: 18),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 12),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF1F1F1),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: TextField(
+                          maxLines: 5,
+                          decoration: InputDecoration(
+                            hintText:
+                                'Add conditions and known symptoms here...',
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: InputBorder.none,
+                            isCollapsed: true,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFFF1F1F1),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.all(12),
-                child: TextField(
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    hintText: 'Add conditions and known symptoms here...',
-                    hintStyle: TextStyle(color: Colors.grey),
-                    border: InputBorder.none,
-                    isCollapsed: true,
+              SizedBox(height: 15),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Account Settings",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                   ),
-                ),
+                  SizedBox(height: 5),
+                  FilledButton(
+                    onPressed: () => print("Implement Manage Subscription"),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.subscriptions, size: 20,),
+                        SizedBox(width: 5),
+                        Text("Manage Subscription", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () => print("Implement Change Sync Account"),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.change_circle, size: 20),
+                        SizedBox(width: 5),
+                        Text(
+                          "Change Sync Account",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () => print("Implement Delete Backup"),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.delete, color: Color(0xffFF0000), size: 20),
+                        SizedBox(width: 5),
+                        Text(
+                          "Delete Backup",
+                          style: TextStyle(
+                            color: Color(0xffFF0000),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
